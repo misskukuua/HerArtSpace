e3813035efd5f32bda92cfd648b148e2f6a06140
tedashikode <theodoremawuli77@gmail.com>
2024-10-14 19:07:13 +0000

    replace react-router-dom with tanstack router

--- frontend/her_art_space/src/App.tsx
+++ frontend/her_art_space/src/App.tsx
@@ -1,15 +1,27 @@
-import SignUpPage from './pages/SignUpPage';
+// import SignUpPage from './pages/SignUpPage';
 
-const App = () => {
+// const App = () => {
 
-    return (
-      <>
-        <main className="flex min-h-screen justify-center sm:items-center sm:pt-0">
-            <SignUpPage />
-        </main>
-      </>
-    );
-};
+//     return (
+//       <>
+//         <main className="flex min-h-screen justify-center sm:items-center sm:pt-0">
+//             <SignUpPage />
+//         </main>
+//       </>
+//     );
+// };
 
-export default App;
+// export default App;
+
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+export default function App() {
+  return <RouterProvider router={router} />
+}
 
